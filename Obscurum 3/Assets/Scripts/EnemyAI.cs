ba514e31f88d65fbd09701ddd0acf5f5ae507293
Dionell Macabugao <dionellmacabugao12@gmail.com>
2019-08-03 21:06:54 -0700

    standard assets, added radius detector for enemies

--- Obscurum 3/Assets/Scripts/EnemyAI.cs
+++ Obscurum 3/Assets/Scripts/EnemyAI.cs
@@ -16,21 +16,34 @@ public class EnemyAI : MonoBehaviour
 
     private Animator anim;
 
+    public float lookRadius = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.speed = speed;
-
+        target = PlayerManager.instance.player.transform;
         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distance = Vector3.Distance(target.position, transform.position);
+
         if(agent.enabled == true)
         {
-            agent.SetDestination(target.position);
+            if(distance <= lookRadius)
+            {
+                agent.SetDestination(target.position);
+            }
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 }
