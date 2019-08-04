97d21167b9604fc7880cca860702f9193b6d511e
Dionell Macabugao <dionellmacabugao12@gmail.com>
2019-08-03 20:01:23 -0700

    enemy following a target

---
+++ Obscurum 3/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent agent;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float health;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.enabled == true)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
